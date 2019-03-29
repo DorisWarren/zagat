26aa32064a9d19033f7aba5ccb0d7e8c999490d2
Doris Warren <doriswarren11@gmail.com>
2019-03-29 16:53:09 -0700

    adding to third component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BackgroundComponent } from './background/background.component';
+import { TheLatestComponent } from './the-latest/the-latest.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    TheLatestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/the-latest/the-latest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-the-latest',
+  templateUrl: './the-latest.component.html',
+  styleUrls: ['./the-latest.component.css']
+})
+export class TheLatestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
