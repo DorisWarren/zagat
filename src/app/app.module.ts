c50d2ae6abb4fa00d140b3e207099c5cbfc00f00
Student <example@example.com>
2019-04-03 11:18:51 -0700

    add connection to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BackgroundComponent } from './background/background.component';
 import { TheLatestComponent } from './the-latest/the-latest.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BackgroundComponent,
     TheLatestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/the-latest/the-latest.component.ts
+++ src/app/the-latest/the-latest.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-the-latest',
   templateUrl: './the-latest.component.html',
-  styleUrls: ['./the-latest.component.css']
+  styleUrls: ['./the-latest.component.css'],
+  providers: [AlbumService]
 })
 export class TheLatestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit() {
   }
